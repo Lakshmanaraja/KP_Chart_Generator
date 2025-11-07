1c40dfc2bcdeed3dff356900df2457f4b01d24b3
lakshmanaraja kannan <lakshmanaraja@gmail.com>
2025-11-07 18:37:48 +0530

    BTR Implem

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
-import swisseph as swe
-import math
-import datetime
-import json
-
 
 app = FastAPI()
 
@@ -19,6 +11,41 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+import swisseph as swe
+import math
+import datetime
+import json
+from pydantic import BaseModel, Field
+from typing import Optional, List, Dict, Any
+import os
+from collections import defaultdict
+import pandas as pd
+import numpy as np
+import json
+import ast
+
+# Flatten
+
+# Define the expected structure for validation
+class TimeRange(BaseModel):
+    minutes: int
+    sign: str
+    description: str
+
+class BTRSubmission(BaseModel):
+    dateOfBirth: str
+    originalBirthTime: str
+    latitude: float
+    longitude: float
+    timezone: str
+    dst: str
+    ayanamsa: str
+    timeRange: TimeRange
+    timeDelta: int
+    originalTimeMatrix: List[List[int]]
+    answerMatrix: List[List[int]]
+    planetOrder: List[str]
 
 
 # ---------------------- Configuration --------------------------------
@@ -348,3 +375,238 @@ def compute_kp_json(date_str:str, time_str:str, lat:float, lon:float, tz_offset_
         })
 
     return out
+
+def unique_list(pl_list):
+
+    seen = set()
+    unique_items = []
+
+    for item in pl_list:
+        if item not in seen:
+            seen.add(item)
+            unique_items.append(item)
+    return(unique_items)
+
+def primary_pl_and_loc_pl(houses,planets):
+    #print(kpjson)
+
+
+        # group planets by house
+    planets_by_house = {}
+    for p in planets:
+        planets_by_house.setdefault(p["house"], []).append(p)
+
+    pr_pl_list = {}
+
+    loc_list = {}
+
+    for h in houses:
+        
+        pl_list = [h['nakshatra_lord'],h['sub_lord'],h['sub_sub_lord']]
+        
+        if len(set(pl_list)) < 3:
+            pl_list.extend(h['sub_sub_sub_lord']) #Filtered SNL
+
+        pr_pl_list[h['house_id']] = unique_list(pl_list)
+
+                
+        plist = planets_by_house.get(h["house_id"], [])
+        plist = sorted(plist, key=lambda x: x["full_degree"])
+
+        for pl in plist:
+            loc_list.setdefault(h['house_id'],[]).append(pl['planet_name'])
+
+    return pr_pl_list,loc_list
+
+
+def calculate_connected_planets(kpjson):
+
+    houses = sorted(kpjson["houses"], key=lambda x: x["house_id"])
+    planets = sorted(kpjson["planets"], key=lambda x: x["house"])
+
+    connected_pl = {}
+    pr_pl_list,loc_list = primary_pl_and_loc_pl(houses,planets)
+
+    combined_pl = {}
+
+    for k in set(pr_pl_list) | set(loc_list):
+        combined_pl[k] = pr_pl_list.get(k, []) + loc_list.get(k, [])
+
+    #print(combined_pl)
+    
+    for h in houses :
+        #print(combined_pl[h['house_id']])
+        connected_pl.setdefault(h['house_id'],[]).extend(combined_pl[h['house_id']])
+        for pr_pl in combined_pl[h['house_id']]:
+            for p in planets :
+                #print(p)
+                if pr_pl in [p['nakshatra_lord'],p['sub_lord']]:
+                    
+                    connected_pl[h['house_id']].append(p['planet_name'])
+                    #break
+                elif (pr_pl in [p['sub_sub_lord']]) & (p['nakshatra_lord']==p['sub_lord']) : #Filtered SSL
+
+                     connected_pl[h['house_id']].append(p['planet_name'])   
+
+
+    for h in houses :
+        connected_pl[h['house_id']] = unique_list(connected_pl[h['house_id']])
+        
+
+    # Step 1: get all unique planet names
+    
+    
+    for pl in VIMSHOTTARI_ORDER:
+        # Step 2: build presence matrix
+        presence = {}
+
+        for house, planets in connected_pl.items():
+            presence[house] = {pl: (1 if pl in planets else 0) for pl in VIMSHOTTARI_ORDER}
+
+        # Step 3: (optional) print as table
+
+    df = pd.DataFrame.from_dict(presence, orient='index')
+
+
+    return df
+
+def calculate_TS_Bhava_significator (birth_date,birth_time,lat,lon,tz,ayan_mode='Lahiri'):
+
+    if EPHE_PATH:
+        swe.set_ephe_path(EPHE_PATH)
+    #time = start_time
+    kpjson = compute_kp_json(birth_date, birth_time, lat, lon, tz, ayan_mode='Lahiri')
+    return(calculate_connected_planets(kpjson))
+    
+def compare_with_answer(df,answer_df):
+
+  
+
+    # Step 1: ensure same dtype (so comparisons behave cleanly)
+    df = df.astype(int)
+    answer_df = answer_df.astype(int)
+
+    mask = (df != 2) & (answer_df != 2) 
+    #print(mask)
+    comparison = (df == answer_df) & mask
+
+    matches = comparison.sum().sum()
+    valid_points = mask.sum().sum()
+    #mismatches = valid_points - matches
+
+
+    # Step 3: find mismatches only within valid mask
+    mismatch_mask = (df != answer_df) & mask
+    # Step 4: get mismatch positions
+    
+    mismatch_locations = [(r + 1, c + 1) for r, c in zip(*np.where(mismatch_mask))]
+    mismatch_values = []
+    for location in mismatch_locations:
+        r = location[0] - 1
+        c = location[1] - 1
+        mismatch_values.append((answer_df.iat[r,c],df.iat[r,c]))
+
+    #print(mismatch_locations)
+    return(matches / valid_points * 100 , mismatch_locations, mismatch_values)
+
+
+@app.get("/api/btr_correction")
+def btr_correction(dateOfBirth:str,originalBirthTime:str,lat:float,lon:float,tz:float,time_delta:int,time_range:int,answer_chart:str,planet_order:str,ayanamsa='Lahiri'):
+    #BTR_Submission = json.loads(data) 
+
+    # birth_time = ["originalBirthTime"]
+    # ayan_mode = BTR_Submission["ayanamsa"]
+    # birth_date = BTR_Submission["dateOfBirth"]
+    # lat = BTR_Submission["latitude"]
+    # lon = BTR_Submission["longitude"]
+    # tz = int(BTR_Submission["timezone"])
+    # answer_chart = BTR_Submission["answerMatrix"]
+
+    birth_date = dateOfBirth
+    birth_time = originalBirthTime
+    start_time = datetime.datetime.strptime(originalBirthTime, "%H:%M:%S") - datetime.timedelta(minutes=int(time_range))
+    end_time = start_time + datetime.timedelta(minutes=int(time_range))
+    step = datetime.timedelta(seconds=int(time_delta))
+
+    #answer_list = json.loads(answer_chart)
+    answer_list = ast.literal_eval(answer_chart)
+    answer_df = pd.DataFrame(answer_list)
+    answer_df.index += 1
+    #answer_df = pd.DataFrame(answer_chart, index=range(1, len(answer_chart)+1))
+
+    # (Optional) Add column names using planetOrder
+    planet_list = ast.literal_eval(planet_order)
+    answer_df.columns = planet_list
+
+    #print(answer_df)
+    #answer_df = pd.DataFrame.from_dict(answer_chart,orient='index',columns=Planet_order)
+
+
+    best_birth_time_list = {}
+    i =0 
+
+    #Loop
+    current = start_time
+    while current <= end_time:
+        #print(current.strftime("%H:%M:%S"))
+        i = i+1
+        birth_time = current.strftime("%H:%M:%S")
+
+        #print(birth_time)
+        df = calculate_TS_Bhava_significator(birth_date,birth_time,lat,lon,tz,ayan_mode='Lahiri')
+        #print(df)
+        percentage ,mismatch_locations, mismatch_values = compare_with_answer(df,answer_df)
+        best_birth_time_list.setdefault(i,[]).append((birth_time,mismatch_locations,percentage,mismatch_values))
+        #print(percentage)
+        current += step
+
+
+    #print(best_birth_time_list[136])
+
+    planet_short_name_list = ['Ke','Ve','Su','Mo','Ma','Ra','Ju','Sa','Me']
+
+    ignore_locations_text = [(3,'Mo'),(2,'Mo')] #[(10,'Ma'),(6,'Mo'),(8,'Ra'),(6,'Ke'),(6,'Sa'),(8,'Su'),(8,'Ju')]  #,,(6,'Mo'),,(8,'Me') (1,'Ra'),(10,'Ma'),(8,'Ra'),(3,'Me')
+
+    ignore_locations = [
+        (row, planet_short_name_list.index(planet) + 1)
+        for row, planet in ignore_locations_text
+    ]
+
+    #print(best_birth_time_list[1][0])
+
+    # Flatten and exclude unwanted mismatches
+    all_items = [
+        (key, time, mismatches, float(score),mismatch_values)
+        for key, lst  in best_birth_time_list.items()
+        for time, mismatches, score , mismatch_values in lst
+        if not any((int(r), int(c)) in ignore_locations for r, c in mismatches)
+    ]
+
+    print(all_items)
+    # Sort and get top 10
+    top_10 = sorted(all_items, key=lambda x: x[3], reverse=True)[:15]
+    print(f"Start Time : {start_time.time()}")
+    print(f"End Time : {end_time.time()}")
+    print(f"Total Number of Charts : {len(best_birth_time_list)}" )
+
+
+
+    results = []
+
+    for key, time, mismatches, score, mismatch_values in top_10:
+        mismatch_str = ", ".join(
+            f"({int(r)}, {planet_short_name_list[int(c) - 1]}, {val1},{val2})"
+            for (r, c), (val1, val2) in zip(mismatches, mismatch_values)
+        )
+        
+        results.append({
+            "chartNo": key,
+            "time": time,
+            "score": round(score, 2),
+            "mismatches": mismatch_str
+        })
+
+
+    # Convert to JSON string (for API or frontend)
+    json_output = json.dumps(results, indent=2)
+    return(json_output)
