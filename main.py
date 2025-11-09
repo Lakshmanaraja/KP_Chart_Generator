854a6a2be3543ec81edb57370db7df787158774c
lakshmanaraja kannan <lakshmanaraja@gmail.com>
2025-11-09 14:31:22 +0530

    fixes in calculate_TS

--- main.py
+++ main.py
@@ -471,7 +471,7 @@ def calculate_connected_planets(kpjson):
     return df
 
 @app.get("/api/calculate_TS_Bhava_Significator")
-def calculate_TS_Bhava_significator (birth_date,birth_time,lat,lon,tz,ayan_mode='Lahiri'):
+def calculate_TS_Bhava_significator (birth_date:str,birth_time:str,lat:float,lon:float,tz:float,ayan_mode='Lahiri'):
 
     if EPHE_PATH:
         swe.set_ephe_path(EPHE_PATH)
